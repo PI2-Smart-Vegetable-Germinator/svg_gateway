0cee1e717c29434e8ce52ca2562ec4ed7b9d6328
Clarissa Borges <clarissalimab08@gmail.com>
2019-10-07 11:02:56 -0300

    Add route to trigger image capture

--- project/api/computer_vision/views.py
+++ project/api/computer_vision/views.py
@@ -3,8 +3,10 @@ import os
 from flask import Blueprint
 from flask import jsonify
 from flask_cors import CORS
+from flask import request
 
 import requests
+import json
 
 computer_vision_blueprint = Blueprint('computer_vision', __name__)
 CORS(computer_vision_blueprint)
@@ -22,6 +24,13 @@ def computer_vision_submit_image():
             'response': 'Image not found!',
         }), 404
     image_file = request.files['file']
-    response = requests.post('%s/api/submit_image', os.getenv('SVG_COMPUTER_VISION_BASE_URI'), files = {'file': image_file})
+    response = requests.post('%s/api/submit_image' % os.getenv('SVG_COMPUTER_VISION_BASE_URI'), data = request.data, files = {'file': image_file})
+
+    return jsonify(response.json()), response.status_code
+
+@computer_vision_blueprint.route('/api/trigger_image_capture', methods=['POST'])
+def computer_vision_trigger_image_capture():
+    url ='http://' + 'localhost' + ':5000/api/take_photo'
+    response = requests.post(url, json=request)
 
     return jsonify(response.json()), response.status_code
